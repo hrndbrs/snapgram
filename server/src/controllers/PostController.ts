bef6983c1eb947960097ed3c4a7842a4b2a5302b
hrndbrs <hrndbrs@gmail.com>
2023-12-14 10:25:25 +0700

    feat: Added endpoints and route handlers
    -get post stats
    -add/remove likes and saves (post stats)

--- server/src/controllers/PostController.ts
+++ server/src/controllers/PostController.ts
@@ -1,6 +1,7 @@
 import { Request, Response, NextFunction } from "express";
 import db from "../db/models";
 import { type TPostCreationAttributes } from "../lib/types";
+import { BaseError } from "../lib/classes";
 
 export default class PostController {
 	static async createNewPost(req: Request, res: Response, next: NextFunction) {
@@ -38,6 +39,97 @@ export default class PostController {
 			});
 
 			res.status(200).json(posts);
+		} catch (err) {
+			next(err);
+		}
+	}
+
+	static async getPostStats(req: Request, res: Response, next: NextFunction) {
+		try {
+			const { id: postId } = req.params;
+
+			const options = { where: { postId }, attributes: ["id", "userId"] };
+
+			const { count, rows: likes } = await db.Like.findAndCountAll(options);
+			const saves = await db.Save.findAll(options);
+
+			res.status(200).json({
+				likes,
+				count,
+				saves,
+			});
+		} catch (err) {
+			next(err);
+		}
+	}
+
+	static async addNewLike(req: Request, res: Response, next: NextFunction) {
+		try {
+			const { id: postId } = req.params;
+			const { id: userId } = (req as Request & { user: { id: string } }).user;
+
+			const payload = { postId, userId };
+
+			const newLike = await db.Like.create(payload);
+
+			res.status(201).json(newLike);
+		} catch (err) {
+			next(err);
+		}
+	}
+
+	static async removeLikeFromPost(
+		req: Request,
+		res: Response,
+		next: NextFunction
+	) {
+		try {
+			const { likeId: id } = req.params;
+			const deletedLike = await db.Like.findByPk(id);
+			if (!deletedLike) throw new BaseError("NotFound", "Like is not found");
+			await db.Like.destroy({
+				where: { id },
+			});
+
+			res.status(200).json(deletedLike);
+		} catch (err) {
+			next(err);
+		}
+	}
+
+	static async addSaveToRecord(
+		req: Request,
+		res: Response,
+		next: NextFunction
+	) {
+		try {
+			const { id: postId } = req.params;
+			const { id: userId } = (req as Request & { user: { id: string } }).user;
+
+			const payload = { postId, userId };
+
+			const newSave = await db.Save.create(payload);
+
+			res.status(201).json(newSave);
+		} catch (err) {
+			next(err);
+		}
+	}
+
+	static async removeSaveFromRecord(
+		req: Request,
+		res: Response,
+		next: NextFunction
+	) {
+		try {
+			const { saveId: id } = req.params;
+			const deletedSave = await db.Save.findByPk(id);
+			if (!deletedSave) throw new BaseError("NotFound", "Record is not found");
+			await db.Save.destroy({
+				where: { id },
+			});
+
+			res.status(200).json(deletedSave);
 		} catch (err) {
 			next(err);
 		}
--- server/src/routers/postRouter.ts
+++ server/src/routers/postRouter.ts
@@ -3,7 +3,13 @@ import PostController from "../controllers/PostController";
 
 const router = express.Router();
 
-router.post("/upload", PostController.createNewPost);
-router.get("/", PostController.getRecentPosts);
+router
+	.get("/", PostController.getRecentPosts)
+	.get("/:id/stats", PostController.getPostStats)
+	.post("/upload", PostController.createNewPost)
+	.post("/:id/likes", PostController.addNewLike)
+	.post("/:id/saves", PostController.addSaveToRecord)
+	.delete("/likes/:likeId", PostController.removeLikeFromPost)
+	.delete("/saves/:saveId", PostController.removeSaveFromRecord);
 
 export default router;
